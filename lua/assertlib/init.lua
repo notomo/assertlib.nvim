342ba784dcdf44fc4c61e8bb51f9b3af3afec15e
notomo <notomo.motono@gmail.com>
2023-10-09 20:32:19 +0900

    Quit vendoring detection

--- lua/assertlib/init.lua
+++ lua/assertlib/init.lua
@@ -1,13 +1,7 @@
 local M = {}
 
-local dir_parts = vim.split((...):gsub("/", "."), ".", { plain = true })
 function M.list()
-  local base_index = vim.fn.index(dir_parts, "assertlib")
-  local base_parts = vim.list_slice(dir_parts, 1, base_index + 1)
-  local base_path = table.concat(base_parts, "/")
-
-  local function_pattern = ("lua/%s/function/*.lua"):format(base_path)
-  local module_paths = vim.api.nvim_get_runtime_file(function_pattern, true)
+  local module_paths = vim.api.nvim_get_runtime_file("lua/assertlib/function/*.lua", true)
   local module_names = vim.tbl_map(function(path)
     path = path:gsub("\\", "/")
     local splitted = vim.split(path, "/", { plain = true })
@@ -16,11 +10,12 @@ function M.list()
     return name
   end, module_paths)
 
+  local from = require("assertlib.asserter").from
+
   local all = {}
-  local base_module_path = table.concat(base_parts, ".")
   for _, module_name in ipairs(module_names) do
-    local module = require(base_module_path .. ".function." .. module_name)
-    local asserters = require(base_module_path .. ".asserter").from(module)
+    local module = require("assertlib.function." .. module_name)
+    local asserters = from(module)
     vim.list_extend(all, asserters)
   end
 
