ddecab878475d8f48f7194c781df463b44d9dc01
notomo <notomo.motono@gmail.com>
2023-10-09 11:25:18 +0900

    Add register()

--- lua/assertlib/init.lua
+++ lua/assertlib/init.lua
@@ -31,4 +31,18 @@ function M.list()
   return all
 end
 
+function M.register(register_fn)
+  local asserters = M.list()
+  for _, asserter in ipairs(asserters) do
+    register_fn(asserter.name, function(self)
+      return function(_, args)
+        local result = asserter.get_result(args)
+        self:set_positive(asserter.positive_message(result))
+        self:set_negative(asserter.negative_message(result))
+        return asserter.is_ok(result)
+      end
+    end)
+  end
+end
+
 return M
